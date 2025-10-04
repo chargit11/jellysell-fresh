38ff3c8ad5c205b404a38a1340cc21ca3f981e35
chargit11 <140365495+chargit11@users.noreply.github.com>
2025-10-04 08:58:11 -0500

    Create page.jsx

---
+++ app/oauth/etsy/callback/page.jsx
@@ -0,0 +1,8 @@
+export default function EtsyCallback() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', fontFamily: 'system-ui' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Connecting to Etsy...</h1>
+      <p>Please wait while we complete the authorization.</p>
+    </div>
+  );
+}
