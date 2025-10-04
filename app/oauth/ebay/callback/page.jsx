fef9dd0f422a352459378ecacfd99fc1393425e8
chargit11 <140365495+chargit11@users.noreply.github.com>
2025-10-04 08:57:51 -0500

    Create page.jsx

---
+++ app/oauth/ebay/callback/page.jsx
@@ -0,0 +1,8 @@
+export default function EbayCallback() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', fontFamily: 'system-ui' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Connecting to eBay...</h1>
+      <p>Please wait while we complete the authorization.</p>
+    </div>
+  );
+}
