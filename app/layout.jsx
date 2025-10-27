fe116907176b88f819856602b9f87e230b09ee78
chargit11 <140365495+chargit11@users.noreply.github.com>
2025-10-27 12:01:40 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,3 @@
-import { SidebarProvider } from '@/contexts/SidebarContext';
 import './globals.css';
 
 export const metadata = {
@@ -10,9 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SidebarProvider>
-          {children}
-        </SidebarProvider>
+        {children}
       </body>
     </html>
   )
