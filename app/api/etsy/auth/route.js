f84b5a225bd35c486a2cb2d04df7d0372cd6f77b
chargit11 <140365495+chargit11@users.noreply.github.com>
2025-11-18 19:53:48 -0600

    Update route.js

--- app/api/etsy/auth/route.js
+++ app/api/etsy/auth/route.js
@@ -13,9 +13,9 @@ export async function GET(req) {
     
     const authUrl = new URL('https://www.etsy.com/oauth/connect');
     authUrl.searchParams.append('response_type', 'code');
-    authUrl.searchParams.append('redirect_uri', process.env.ETSY_REDIRECT_URI);
+    authUrl.searchParams.append('redirect_uri', 'https://jellysell.com/api/etsy/callback');
     authUrl.searchParams.append('scope', 'listings_r listings_w transactions_r email_r shops_r');
-    authUrl.searchParams.append('client_id', process.env.ETSY_CLIENT_ID);
+    authUrl.searchParams.append('client_id', 'zmldauxi8u7zz87qztr6l64x');
     authUrl.searchParams.append('state', state);
     authUrl.searchParams.append('code_challenge', 'code_challenge');
     authUrl.searchParams.append('code_challenge_method', 'S256');
